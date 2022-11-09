3659bef7a532ed348dcec27f793f9d846deb90a5
nomekop65 <64111362+nomekop65@users.noreply.github.com>
2022-11-09 15:34:59 -0500

    Add files via upload

---
+++ Assignment8/src/assignment8/Assignment8.java
@@ -0,0 +1,36 @@
+package assignment8;
+
+import java.util.Scanner;
+
+/*
+My words and actions will reflect Academic Integrity. 
+I will not cheat or lie or steal in academic matters. 
+I will promote integrity in the UNCG community. 
+Student’s Signature Arefeh Abbaszadeh, Siming Jia, Kevin Ludwig, Damien Phantha, and Kannan Thilak Date 11/9/22
+*/
+
+
+public class Assignment8 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public static int password( ){
+        return 0;
+    }
+    
+    public static void getCountry(){
+    
+    }
+    
+    public static String showCountry(){
+        return "";
+    }
+    
+    public static String registerScreen(){
+        return "";
+    }
+    
+    
+}
